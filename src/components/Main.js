3c4b6346fb931b99f70613363bf371c58c159d7c
Kevin Souto <kevinsoutods@gmail.com>
2022-06-16 23:58:47 -0300

    QuartaAula_EstiloCSS1

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,38 @@
 import React, { Component } from "react";
 
+//form
+import { FaPlus } from 'react-icons/fa';
+
+import './Main.css';
+
 export default class Main extends Component {
-  constructor(props) {
-    super(props);
+  //class field
+  state = {
+    novaTarefa: '',
+  };
 
-    this.state = {
-
-    };
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
   }
 
   render() {
+    const {novaTarefa} = this.state;
+
     return (
       <div className="main">
         <h1>Lista de Tarefas</h1>
 
-        <form action="#">
-          <input type="text" placeholder="insert your task"/>
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={novaTarefa}
+          />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
       </div>
     );
