bc662d03b76e922018c83024282a3cf37f1a963b
Kevin Souto <kevinsoutods@gmail.com>
2022-06-16 23:23:41 -0300

    TerceiraAula_Boilerplate

---
+++ src/components/Main.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <h1>Lista de Tarefas</h1>
+
+        <form action="#">
+          <input type="text" placeholder="insert your task"/>
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    );
+  }
+}
