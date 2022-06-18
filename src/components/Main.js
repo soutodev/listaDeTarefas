55b872986cabc3f111aca7f9eb30f945ac265bf9
Kevin Souto <kevinsoutods@gmail.com>
2022-06-17 23:43:05 -0300

    aulaReact_EditTask

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,11 +16,12 @@ export default class Main extends Component {
   state = {
     novaTarefa: '',
     tarefas: [ ],
+    index: -1,
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const { tarefas } = this.state;
+    const { tarefas, index } = this.state;
     let { novaTarefa } = this.state;
     novaTarefa = novaTarefa.trim();
 
@@ -28,10 +29,19 @@ export default class Main extends Component {
 
     const novasTarefas = [...tarefas];
 
-    this.setState({
-      tarefas: [...novasTarefas, novaTarefa],
-      novaTarefa: '',
-    });
+    if (index == -1) {
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: '',
+      });
+    } else {
+      novasTarefas[index] = novaTarefa;
+
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1,
+      });
+    }
   }
 
   handleChange = (e) => {
@@ -41,7 +51,12 @@ export default class Main extends Component {
   }
 
   handleEdit = (e, index) => {
-    console.log('Edit', index);
+    const { tarefas } = this.state;
+
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    });
   }
 
   handleDelete = (e, index) => {
